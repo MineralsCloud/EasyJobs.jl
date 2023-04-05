06576c5276b7294bda142bb22e2b36cec8652517
singularitti <singularitti@outlook.com>
2023-04-05 02:34:18 -0400

    Update `chain!` in src/operations.jl

--- EasyJobsBase/src/operations.jl
+++ EasyJobsBase/src/operations.jl
@@ -9,11 +9,16 @@ Chain multiple `Job`s one after another.
 function chain!(x::AbstractJob, y::AbstractJob)
     if x == y
         throw(ArgumentError("a job cannot be followed by itself!"))
+    end
+    if y in x.children && x in y.parents
+        @info "You cannot chain the same jobs twice! No operations will be done!"
+    elseif y in x.children || x in y.parents  # This should never happen
+        error("Only one job is linked to the other, something is wrong!")
     else
         push!(x.children, y)
         push!(y.parents, x)
-        return x
     end
+    return x
 end
 chain!(x::AbstractJob, y::AbstractJob, z::AbstractJob...) = foldr(chain!, (x, y, z...))
 """
