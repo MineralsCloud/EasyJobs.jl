ef36c22d2f29744d71d763c786c6f584ca067bdf
singularitti <singularitti@outlook.com>
2023-04-24 18:23:50 -0400

    Fix `show` for src/show.jl

--- EasyJobsBase/src/show.jl
+++ EasyJobsBase/src/show.jl
@@ -1,29 +1,32 @@
+# See https://docs.julialang.org/en/v1/manual/types/#man-custom-pretty-printing
 function Base.show(io::IO, job::AbstractJob)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(job)
-        Base.show_default(IOContext(io, :limit => true), job)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
+    if get(io, :compact, false)
+        print(
+            IOContext(io, :limit => true, :compact => true), summary(job), '(', job.id, ')'
+        )
     else
-        println(io, summary(job))
-        println(io, ' ', "id: ", job.id)
-        if !isempty(job.description)
-            print(io, ' ', "description: ")
-            show(io, job.description)
-            println(io)
-        end
-        print(io, ' ', "def: ")
-        print(io, job.def)
-        print(io, '\n', ' ', "status: ")
-        printstyled(io, getstatus(job); bold=true)
-        if !ispending(job)
-            println(
-                io, '\n', ' ', "from: ", format(starttimeof(job), "dd-u-YYYY HH:MM:SS.s")
-            )
-            print(io, ' ', "to: ")
-            if isrunning(job)
-                print(io, "still running...")
-            else
-                # println(io, format(stoptime(job), "dd-u-YYYY HH:MM:SS.s"))
-                print(io, ' ', "uses: ", timecostof(job))
-            end
+        print(io, summary(job), '(', job.id, "), ", job.def)
+    end
+end
+function Base.show(io::IO, ::MIME"text/plain", job::AbstractJob)
+    println(io, summary(job))
+    println(io, ' ', "id: ", job.id)
+    if !isempty(job.description)
+        print(io, ' ', "description: ")
+        show(io, job.description)
+        println(io)
+    end
+    print(io, ' ', "def: ")
+    print(io, job.def)
+    print(io, '\n', ' ', "status: ")
+    printstyled(io, getstatus(job); bold=true)
+    if !ispending(job)
+        println(io, '\n', ' ', "from: ", format(starttimeof(job), "HH:MM:SS.s, mm/dd/yyyy"))
+        if isrunning(job)
+            print(io, " still running...")
+        else
+            println(io, ' ', "to: ", format(endtimeof(job), "HH:MM:SS.s, mm/dd/yyyy"))
+            print(io, ' ', "used: ", timecostof(job))
         end
     end
 end
