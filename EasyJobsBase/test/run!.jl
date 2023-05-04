31f6c044a68c95fd8c54b8c11cb28d64f440174a
singularitti <singularitti@outlook.com>
2023-05-04 03:27:19 -0400

    Fix tests in test/run!.jl

--- EasyJobsBase/test/run!.jl
+++ EasyJobsBase/test/run!.jl
@@ -37,8 +37,8 @@ using Thinkers
         m = Job(Thunk(f₅, 3, 1); name="m")
         n = Job(Thunk(f₆, 1; x=3); username="she", name="n")
         for job in (i, j, k, l, m, n)
-            run!(job)
-            wait(job)
+            exe = run!(job)
+            wait(exe)
             @test issucceeded(job)
         end
     end
@@ -49,52 +49,64 @@ using Thinkers
         l = Job(Thunk(f₄); name="l", username="me")
         m = Job(Thunk(f₅, 3, 1); name="m")
         n = Job(Thunk(f₆, 1; x=3); username="she", name="n")
-        i ⇉ [j, k] ⇶ [l, m] ⭃ n
+        Ref(i) .→ [j, k] .→ [l, m] .→ Ref(n)
+        @assert isempty(i.parents)
+        @assert i.children == Set([j, k])
+        @assert j.parents == Set([i])
+        @assert j.children == Set([l])
+        @assert k.parents == Set([i])
+        @assert k.children == Set([m])
+        @assert l.parents == Set([j])
+        @assert l.children == Set([n])
+        @assert m.parents == Set([k])
+        @assert m.children == Set([n])
+        @assert n.parents == Set([l, m])
+        @assert isempty(n.children)
         for job in (i, j, k, l, m, n)
-            run!(job)
-            wait(job)
+            exe = run!(job)
+            wait(exe)
             @test issucceeded(job)
         end
     end
 end
 
-# @testset "Test running `SubsequentJob`s" begin
-#     f₁(x) = write("file", string(x))
-#     f₂() = read("file", String)
-#     i = Job(Thunk(f₁, 1001); username="me", name="i")
-#     j = SubsequentJob(Thunk(map, f₂); username="he", name="j")
-#     i → j
-#     @test_throws AssertionError run!(j)
-#     @test getresult(j) === nothing
-#     run!(i)
-#     wait(i)
-#     run!(j)
-#     wait(j)
-#     @test getresult(j) == Some("1001")
-# end
+@testset "Test running `WeaklyDependentJob`s" begin
+    f₁(x) = write("file", string(x))
+    f₂() = read("file", String)
+    i = Job(Thunk(f₁, 1001); username="me", name="i")
+    j = WeaklyDependentJob(Thunk(map, f₂); username="he", name="j")
+    i → j
+    @test_throws AssertionError run!(j)
+    @test getresult(j) === nothing
+    exe = run!(i)
+    wait(exe)
+    exe = run!(j)
+    wait(exe)
+    @test getresult(j) == Some("1001")
+end
 
-@testset "Test running piped jobs" begin
+@testset "Test running `StronglyDependentJob`s" begin
     f₁(x) = x^2
     f₂(y) = y + 1
     f₃(z) = z / 2
     i = Job(Thunk(f₁, 5); username="me", name="i")
-    j = DependentJob(Thunk(f₂, 3); username="he", name="j")
-    k = DependentJob(Thunk(f₃, 6); username="she", name="k")
-    i ⇒ j ⇒ k
+    j = StronglyDependentJob(Thunk(f₂, 3); username="he", name="j")
+    k = StronglyDependentJob(Thunk(f₃, 6); username="she", name="k")
+    i → j → k
     @test_throws AssertionError run!(j)
-    run!(i)
-    wait(i)
+    exe = run!(i)
+    wait(exe)
     @test getresult(i) == Some(25)
     @test_throws AssertionError run!(k)
-    run!(j)
-    wait(j)
+    exe = run!(j)
+    wait(exe)
     @test getresult(j) == Some(26)
-    run!(k)
-    wait(k)
+    exe = run!(k)
+    wait(exe)
     @test getresult(k) == Some(13.0)
 end
 
-@testset "Test running a piped job with more than one parents" begin
+@testset "Test running a `StronglyDependentJob` with more than one parent" begin
     f₁(x) = x^2
     f₂(y) = y + 1
     f₃(z) = z / 2
@@ -103,18 +115,17 @@ end
     i = Job(Thunk(f₁, 5); username="me", name="i")
     j = Job(Thunk(f₂, 3); username="he", name="j")
     k = Job(Thunk(f₃, 6); username="she", name="k")
-    l = DependentJob(Thunk(f₄, ()); username="she", name="me")
-    h → l  # Ignore the output of this job
+    l = StronglyDependentJob(Thunk(f₄, ()); username="she", name="me")
     for job in (i, j, k)
-        job ⇒ l
+        job → l
     end
     @test_throws AssertionError run!(l)
     for job in (h, i, j, k)
-        run!(job)
-        wait(job)
+        exe = run!(job)
+        wait(exe)
     end
-    run!(l)
-    wait(l)
+    exe = run!(l)
+    wait(exe)
     @test getresult(i) == Some(25)
     @test getresult(j) == Some(4)
     @test getresult(k) == Some(3.0)
