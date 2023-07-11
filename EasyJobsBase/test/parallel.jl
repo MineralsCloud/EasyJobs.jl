09852a9c203dd3c47baae06207b8db09ae3eea9a
singularitti <singularitti@outlook.com>
2023-07-11 01:31:03 -0400

    Fix "Test `execute!` for `ParallelExecutor`"

--- EasyJobsBase/test/parallel.jl
+++ EasyJobsBase/test/parallel.jl
@@ -4,17 +4,25 @@ addprocs(3)
 @everywhere using EasyJobsBase
 @everywhere using EasyJobsBase: ParallelExecutor, runonce!
 
-@testset "Test `execute!` returns different `Job`s for `ParallelExecutor`" begin
+@testset "Test `execute!` for `ParallelExecutor`" begin
     i = Job(Thunk(myid); username="me", name="i")
     exec = ParallelExecutor(2; wait=true)
     task1 = execute!(i, exec)
-    newjob = fetch(task1)
-    @test newjob != i
-    @test getresult(newjob) == Some(2)
-    @testset "Test `execute!` returns a different `Job` when succeeded for `ParallelExecutor`" begin
+    i′ = fetch(task1)
+    @test ispending(i)  # `i` is not executed
+    @test issucceeded(i′)  # `i′` is a copy of `i` and was executed
+    @test getresult(i′) == Some(2)
+    @test i′ != i
+    @testset "Test `execute!` returns a different `Job` for `ParallelExecutor`" begin
         task2 = execute!(i, exec)
         @test task1 != task2
-        @test fetch(task1) != fetch(task2)
+        @test i′ != fetch(task2)
+    end
+    @testset "Test `execute!` returns the same `Job` when succeeded for `ParallelExecutor`" begin
+        task2 = execute!(i′, exec)
+        @test i′ == fetch(task2)
+        task3 = execute!(i′, exec)
+        @test i′ == fetch(task3)
     end
 end
 
