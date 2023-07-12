3d8a52f74d079f6f330ef113a91a39a5f98585ed
singularitti <singularitti@outlook.com>
2023-07-11 23:22:07 -0400

    Fix `run!` method to distinguish `AsyncExecutor` & `ParallelExecutor`

--- EasyJobsBase/src/run.jl
+++ EasyJobsBase/src/run.jl
@@ -45,15 +45,14 @@ end
 
 """
     run!(job::Job; maxattempts=1, interval=1, delay=0, wait=false)
-    run!(job::Job, style::Async; maxattempts=1, interval=1, delay=0, wait=false)
-    run!(job::Job, style::Parallel; maxattempts=1, interval=1, delay=0, wait=false)
+    run!(job::Job, worker::Integer; maxattempts=1, interval=1, delay=0, wait=false)
 
 Run a `Job` with a maximum number of attempts, with each attempt separated by `interval` seconds
 and an initial `delay` in seconds.
 """
-run!(job::AbstractJob, ::Async; kwargs...) = execute!(job, AsyncExecutor(; kwargs...))
-run!(job::AbstractJob, ::Parallel; kwargs...) = execute!(job, ParallelExecutor(; kwargs...))
-run!(job::AbstractJob; kwargs...) = run!(job, Async(); kwargs...)
+run!(job::AbstractJob; kwargs...) = execute!(job, AsyncExecutor(; kwargs...))
+run!(job::AbstractJob, worker::Integer; kwargs...) =
+    execute!(job, ParallelExecutor(worker; kwargs...))
 
 """
     execute!(job::AbstractJob, exec::Executor)
